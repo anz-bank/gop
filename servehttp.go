03c8989a8cea69202b9b060b31b3ffdd9d1b01fb
joshcarp <joshuacarpeggiani@gmail.com>
2020-09-14 12:46:03 +1000

    Add cors function

--- servehttp.go
+++ servehttp.go
@@ -119,3 +119,17 @@ func NewGopper(a app.AppConfig) (*GitGopper, error) {
 	r.Retriever = retriever_git.New(a)
 	return &r, nil
 }
+
+func CORSEnabledFunction(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+}
