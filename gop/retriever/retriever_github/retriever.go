94231eb118e98697406279fa5eb1e06a95278def
joshcarp <joshuacarpeggiani@gmail.com>
2020-09-17 14:23:00 +1000

    Add support for enterprise github apis

--- gop/retriever/retriever_github/retriever.go
+++ gop/retriever/retriever_github/retriever.go
@@ -18,27 +18,46 @@ func New(token string) Retriever {
 	return Retriever{token: token}
 }
 
-func (a Retriever) Retrieve(resource string) (res []byte, cached bool, err error) {
+func (a Retriever) Retrieve(resource string) ([]byte, bool, error) {
 	var resp *http.Response
-	repo, resource, version, err := gop.ProcessRequest(resource)
+	var apibase string
+	var repo, path, version string
+	var err error
+	var res []byte
+
+	repo, path, version, err = gop.ProcessRequest(resource)
 	if err != nil {
 		return nil, false, gop.CreateError(gop.BadRequestError, "Can't process request")
 	}
+	requestedurl, _ := url.Parse("https://" + resource)
+	host := requestedurl.Host
+	repo = strings.ReplaceAll(repo, host+"/", "")
 
-	req, err := url.Parse(fmt.Sprintf("https://api.github.com/repos/%s/contents/%s?ref=%s",
-		strings.ReplaceAll(repo, "github.com/", ""), resource, version))
-	cl := http.DefaultClient
+	switch host {
+	case "github.com":
+		apibase = "api.github.com"
+	default:
+		apibase = fmt.Sprintf("%s/api/v3", host)
+	}
+
+	req, err := url.Parse(
+		fmt.Sprintf(
+			"https://%s/repos/%s/contents/%s?ref=%s",
+			apibase, repo, path, version))
 	heder := http.Header{}
 	heder.Add("accept", "application/vnd.github.v3.raw+json")
+
 	if a.token != "" {
 		heder.Add("authorization", "token "+a.token)
 	}
+
 	r := &http.Request{
 		Method: "GET",
 		URL:    req,
 		Header: heder,
 	}
-	resp, err = cl.Do(r)
+
+	resp, err = http.DefaultClient.Do(r)
 	if err != nil {
 		return res, false, err
 	}
